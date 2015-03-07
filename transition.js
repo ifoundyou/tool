f43cfcf8f31c5ef91786964920ed293383da78e6
Jack <triumphant@163.com>
2015-03-07 23:48:40 +0800

    add css3.css and transtion.js

---
+++ transition.js
@@ -0,0 +1,22 @@
+/* From Modernizr */
+function whichTransitionEvent() {
+    var t;
+    var el = document.createElement('fakeelement');
+    var transitions = {
+        'transition': 'transitionend',
+        'OTransition': 'oTransitionEnd',
+        'MozTransition': 'transitionend',
+        'WebkitTransition': 'webkitTransitionEnd'
+    }
+
+    for (t in transitions) {
+        if (el.style[t] !== undefined) {
+            return transitions[t];
+        }
+    }
+}
+
+// var transitionEvent = whichTransitionEvent();
+// transitionEvent && e.addEventListener(transitionEvent, function() {
+//   console.log('Transition complete!  This is the callback, no library needed!');
+// });
